3cec4e9ec9a6210bb9def0aedc4f08591574cf66
ayodeji.ilori <ayodeji.ilori@globalaccelerex.com>
2020-03-07 11:30:02 +0100

    Work In Progress

---
+++ session1/src/main/scala/com/lagosscala/PatternMatchingEx.scala
@@ -0,0 +1,35 @@
+package com.lagosscala
+
+object PatternMatchingEx {
+
+  //simple constant patterns ( match is an expression )
+  def matchIt(x : Any) : Unit = ???
+
+  //variable loads
+  def wordsAndOpposite(word : String): String = ???
+
+  def wordsAndOpposite2(word : String) : String  = ???
+
+  //Typed Pattern Matches
+  /*
+  * object obj = "A String Value";
+  * if(obj instanceOf String) { do something }
+  */
+  def describeType(x : Any) = ???
+
+  //Guards - Adds a predicate to a pattern match on the left side
+  def describeNumber(num : Int) : String = ???
+
+
+
+  //match tuples
+  def matchTuple3(tup3 : (Int , Boolean , String)) : String = ???
+
+  // pattern matching with case classes
+
+
+
+  //pattern match with Val on tuples / case class ( Extractors )
+
+  //for and pattern matching
+}
