b492d0741c716cb8e27daa19814e7f3dc50b649c
ayodeji.ilori <ayodeji.ilori@globalaccelerex.com>
2020-03-07 14:08:10 +0100

    Work In Progress - Completed VariableTypes

--- session1/src/main/scala/com/lagosscala/VariablesTypesEx.scala
+++ session1/src/main/scala/com/lagosscala/VariablesTypesEx.scala
@@ -1,5 +1,30 @@
 package com.lagosscala
 
-object VariablesTypesEx {
+object VariablesTypesEx  extends App {
 
+  //In Scala you cannot simply create a variable and leave it un-initialized.
+  //Mutable and Immutable Variables
+  val anInt = 34;
+
+  val aDouble = 23.0
+
+  val strValue = "Hello"
+
+  println(anInt + 34)
+
+  //will not work
+ // println(aDouble / "justastring")
+
+  //Declaring a Tuple
+  val ingredient = ("Sugar" , 25)
+
+  //Accessing the Elements of the Tuple
+  println(ingredient._1) // Sugar
+  println(ingredient._2) // 25
+
+
+  //Pattern Matching on Tuple
+  val (name, quantity) = ingredient
+  println(name) // Sugar
+  println(quantity) // 25
 }
